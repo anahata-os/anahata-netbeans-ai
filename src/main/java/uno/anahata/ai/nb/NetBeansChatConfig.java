49b4ec485681d71afcf6605ce806732d9b99f621
Your Name <you@example.com>
2025-12-12 21:49:01 +0100

    putting netbeans core instructions provider first

--- src/main/java/uno/anahata/ai/nb/NetBeansChatConfig.java
+++ src/main/java/uno/anahata/ai/nb/NetBeansChatConfig.java
@@ -38,7 +38,7 @@ public class NetBeansChatConfig extends SwingChatConfig {
 
     public NetBeansChatConfig(String sessionUuid) {
         this.sessionUuid = sessionUuid;
-        providers.add(new CoreNetBeansInstructionsProvider());
+        providers.add(0, new CoreNetBeansInstructionsProvider());
         providers.add(new OpenTopComponentsContextProvider());
         providers.add(new OutputTabsContextProvider());
     }
