b25bbd3b9d3d236d6e020dd93af833db9b193d75
Your Name <you@example.com>
2025-12-12 21:49:20 +0100

    license

--- src/main/java/uno/anahata/ai/nb/model/java2/JavaMember.java
+++ src/main/java/uno/anahata/ai/nb/model/java2/JavaMember.java
@@ -1,5 +1,5 @@
 /*
- * Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Fora Bara!
+ * Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça!
  */
 package uno.anahata.ai.nb.model.java2;
 
